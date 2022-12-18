419dd361902fa0ac795e3f480925e21b94f6d714
CarlesRa <jcramos8282@gmail.com>
2022-12-18 23:09:37 +0100

    add new toast keys

--- src/app/types/toast.types.ts
+++ src/app/types/toast.types.ts
@@ -1,2 +1,2 @@
-export type ToastKey = 'tl' | 'tc' | 'tr' | 'bl' | 'bc' | 'br';
+export type ToastKey = 'tl' | 'tc' | 'tr' | 'cl' | 'cc' | 'cr' | 'bl' | 'bc' | 'br';
 export type ToastSeverity = 'success' | 'info' | 'warn' | 'error';
