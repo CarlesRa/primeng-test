c89bef563b077bfeccb204c0d98c1e8e618b72ad
CarlesRa <jcramos8282@gmail.com>
2022-12-18 18:26:48 +0100

    remove unused import

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { MessageService } from 'primeng/api';
 import { showToast$ } from '../shared/toast/toast.component';
 
 @Component({
